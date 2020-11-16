a52e32b5bf5fa01a95ca98937b167685171874de
mmazepa <maksymilianmariusz@gmail.com>
2020-11-16 21:39:06 +0100

    Upgrading "Hello World!".

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,2 +1,10 @@
-var message = "Hello World!";
-console.log(message);
+var Greeting = /** @class */ (function () {
+    function Greeting() {
+    }
+    Greeting.prototype.greet = function () {
+        console.log("Hello World!");
+    };
+    return Greeting;
+}());
+var message = new Greeting();
+message.greet();
--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,2 +1,8 @@
-var message:string = "Hello World!"
-console.log(message)
+class Greeting {
+   greet():void {
+      console.log("Hello World!")
+   }
+}
+
+var message = new Greeting();
+message.greet();
